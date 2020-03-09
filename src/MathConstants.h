c7b109a4e5b6d9bf564ea6551f4b376139acc186
2bit <i@2bit.jp>
2020-03-09 21:49:16 +0900

    replace CPP to constexpr

--- src/MathConstants.h
+++ src/MathConstants.h
@@ -1,45 +1,79 @@
-#ifndef PI
-    #define PI       3.14159265358979323846
+#include <type_traits>
+
+#ifdef PI
+#   undef PI
 #endif
 
-#ifndef TWO_PI
-    #define TWO_PI   6.28318530717958647693
+static constexpr double PI{3.14159265358979323846};
+
+#ifdef TWO_PI
+#   undef TWO_PI
 #endif
 
-#ifndef M_TWO_PI
-    #define M_TWO_PI   6.28318530717958647693
+static constexpr double TWO_PI{6.28318530717958647693};
+
+#ifdef M_TWO_PI
+#   undef M_TWO_PI
 #endif
 
-#ifndef FOUR_PI
-    #define FOUR_PI 12.56637061435917295385
+static constexpr double M_TWO_PI{6.28318530717958647693};
+
+#ifdef FOUR_PI
+#   undef FOUR_PI
 #endif
 
-#ifndef HALF_PI
-    #define HALF_PI  1.57079632679489661923
+static constexpr double FOUR_PI{12.56637061435917295385};
+
+#ifdef HALF_PI
+#   undef HALF_PI
 #endif
 
-#ifndef DEG_TO_RAD
-    #define DEG_TO_RAD (PI/180.0)
+static constexpr double HALF_PI{1.57079632679489661923};
+
+#ifdef DEG_TO_RAD
+#   undef DEG_TO_RAD
 #endif
+
+static constexpr double DEG_TO_RAD{PI / 180.0};
 
 #ifndef RAD_TO_DEG
-    #define RAD_TO_DEG (180.0/PI)
+#   undef RAD_TO_DEG
 #endif
 
-#ifndef MIN
-    #define MIN(x,y) (((x) < (y)) ? (x) : (y))
+static constexpr double RAD_TO_DEG{180.0 / PI};
+
+#ifdef MIN
+#   undef MIN
 #endif
 
-#ifndef MAX
-    #define MAX(x,y) (((x) > (y)) ? (x) : (y))
+template <typename t1, typename t2>
+static inline constexpr typename std::common_type<t1, t2>::type MIN(t1 x, t2 y) {
+    return (x < y) ? x : y;
+}
+
+#ifdef MAX
+#   undef MAX
 #endif
 
-#ifndef CLAMP
-    #define CLAMP(val,min,max) ((val) < (min) ? (min) : ((val > max) ? (max) : (val)))
+template <typename t1, typename t2>
+static inline constexpr typename std::common_type<t1, t2>::type MAX(t1 x, t2 y) {
+    return (x < y) ? y : x;
+}
+
+#ifdef CLAMP
+#   undef CLAMP
 #endif
 
-#ifndef ABS
-    #define ABS(x) (((x) < 0) ? -(x) : (x))
+template <typename t1, typename t2, typename t3>
+static inline constexpr typename std::common_type<t1, t2, t3>::type CLAMP(t1 val, t2 min_v, t3 max_v) {
+    return MIN(max_v, MAX(min_v, val));
+}
+
+#ifdef ABS
+#   undef ABS
 #endif
 
-
+template <typename type>
+static inline constexpr type ABS(type x) {
+    return (x < 0) ? -x : x;
+}
